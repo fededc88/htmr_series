201bcdf853a43d5615aa12726c2777149ad54620
Federico D. Ceccarelli <fededc88@gmail.com>
2025-07-06 23:08:50 -0400

    added argparser to pass url as argument

--- series.py
+++ series.py
@@ -2,40 +2,57 @@ from bs4 import BeautifulSoup
 import requests
 
 import pandas as pd
-
-url = 'https://viberc.liverc.com/results/?p=view_race_result&id=6000488'
-r = requests.get(url)
-
-#print(r.text)
-
-soup = BeautifulSoup(r.text, 'html.parser')
-
-soup_table = soup.find('table', class_="table table-striped race_result")
-#print(soup_table)
-
-titles = [title.text for title in soup_table.find_all('th')[1:14] ]
-
-tbody = soup_table.find('tbody')
-
-data = [[data.text for data in line.find_all('td')] for line in tbody.find_all('tr')]
-
-#print(data)
-
-#print(titles)
-
-df = pd.DataFrame(columns = titles)
-
-for line in data:
-    df.loc[len(df)] = line
-
-print(df)
+import argparse
 
 
+def getargs():
 
-#names = soup_table.find_all('th')
-#print(names)
-#
-#
-#names = soup_table.find_all('span', class_ ='driver_name').text
-#print(names);
+    parser = argparse.ArgumentParser(
+                    prog='ProgramName',
+                    description='What the program does',
+                    epilog='Text at the bottom of help')
+    
+    parser.add_argument('url',
+            type = str,
+            help = 'LiveRC race result url')
+
+    return parser.parse_args()
+
+def main():
+
+    args = getargs()
+
+    print(args.url)
+
+    r = requests.get(args.url)
+    
+    #print(r.text)
+    
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    soup_table = soup.find('table', class_="table table-striped race_result")
+    #print(soup_table)
+    
+    titles = [title.text for title in soup_table.find_all('th')[1:14] ]
+    
+    tbody = soup_table.find('tbody')
+    
+    data = [[data.text for data in line.find_all('td')] for line in tbody.find_all('tr')]
+    
+    #print(data)
+    
+    #print(titles)
+    
+    df = pd.DataFrame(columns = titles)
+    
+    for line in data:
+        df.loc[len(df)] = line
+    
+    print(df)
+
+if __name__ == '__main__':
+    main()
+
+
+
 
